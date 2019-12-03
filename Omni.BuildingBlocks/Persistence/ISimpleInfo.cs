689fe263eb3014fd40d20f6438d731bf598a516e
Bradford Foxworth-Hill <Bradford.foxworthhill@gmail.com>
2019-12-03 17:15:53 -0500

    added SimpleInfo interface

---
+++ Omni.BuildingBlocks/Persistence/ISimpleInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Omni.BuildingBlocks.Persistence
+{
+    public interface ISimpleInfo
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+    }
+}
---
+++ Omni.BuildingBlocks/Shared/ISimpleInfoModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Omni.BuildingBlocks.Shared
+{
+    public interface ISimpleInfo
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+    }
+}
