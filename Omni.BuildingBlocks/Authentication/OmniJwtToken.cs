60be52cbce527c495c98a3aeba703c008b2a866a
Bradford Foxworth-Hill <Bradford.foxworthhill@gmail.com>
2019-12-11 23:26:30 -0500

    updating configuration models

--- Omni.BuildingBlocks/Authentication/OmniJwtToken.cs
+++ Omni.BuildingBlocks/Authentication/OmniJwtToken.cs
@@ -1,9 +1,14 @@
-﻿namespace Omni.BuildingBlocks.Authentication
+﻿using Newtonsoft.Json;
+
+namespace Omni.BuildingBlocks.Authentication
 {
     public class OmniJwtToken
     {
+        [JsonProperty("access_token")]
         public string AccessToken { get; set; }
+        [JsonProperty("refresh_token")]
         public string RefreshToken { get; set; }
+        [JsonProperty("id_token")]
         public string IdToken { get; set; }
     }
 }
