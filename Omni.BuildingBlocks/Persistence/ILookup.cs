f3a3743a2dbf0e61e512024ce38f59822838f4dc
Bradford Foxworth-Hill <Bradford.foxworthhill@gmail.com>
2020-06-02 00:51:34 -0400

    Added Lookup Models

---
+++ Omni.BuildingBlocks/Persistence/ILookup.cs
@@ -0,0 +1,9 @@
+﻿namespace Omni.BuildingBlocks.Shared
+{
+    public interface ILookup<T>
+    {
+        T Id { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+    }
+}
---
+++ Omni.BuildingBlocks/Shared/ILookupModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Omni.BuildingBlocks.Shared
+{
+    public interface ILookupModel
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+    }
+}
